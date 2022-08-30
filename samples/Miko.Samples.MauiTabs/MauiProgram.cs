463f2b03efde081debba1e601d32eee9e3e0febe
zhaowenkai <799480165@qq.com>
2022-08-30 14:50:05 +0800

    add maui tabs sample

---
+++ samples/Miko.Samples.MauiTabs/MauiProgram.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace Miko.Samples.MauiTabs
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		    builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddMiko(options =>
+            {
+                options.Mode = GetMode();
+            });
+
+            return builder.Build();
+        }
+
+        public static string GetMode()
+        {
+            if (DeviceInfo.Current.Platform == DevicePlatform.Android)
+            {
+                return "md";
+            }
+
+            if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+            {
+                return "ios";
+            }
+
+            return "md";
+        }
+    }
+}
