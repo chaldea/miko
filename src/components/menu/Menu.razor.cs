f3ea1c8ef89e226f7accdf91eac40471efb9c85e
zhaowenkai <799480165@qq.com>
2022-09-01 13:18:07 +0800

    add components

---
+++ src/components/menu/Menu.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Miko
+{
+    public partial class Menu
+    {
+        private bool isPaneVisible = false;
+        private bool isEndSide = false;
+
+        [Parameter] public string ContentId { get; set; }
+        [Parameter] public string Type { get; set; } = "overlay";
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public string Side { get; set; } = "start";
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            SetClassMap();
+        }
+
+        protected void SetClassMap()
+        {
+            ClassMapper
+                .Clear()
+                .Add(Mode)
+                .Add($"menu-type-{Type}")
+                .If("menu-enabled", () => !Disabled)
+                .If("menu-side-end", () => isEndSide)
+                .If("menu-side-start", () => !isEndSide)
+                .If("menu-pane-visible", () => !isPaneVisible);
+        }
+    }
+}
