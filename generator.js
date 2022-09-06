98a26743c163c5a8403a5163a1550e849a561e51
zhaowenkai <799480165@qq.com>
2022-09-06 13:41:28 +0800

    fix button style conflict

--- generator.js
+++ generator.js
@@ -7,6 +7,8 @@ const slotted2 = /(?:\):):slotted\(([\s\S]*?)\)(?!\))/gm;   // match ::slotted
 const slotted3 = /::slotted\(([\s\S]*?)\)(?!\))/gm;         // match ::slotted
 const host1 = /:host-context\(([\s\S]*?)\)(?!\))/gm;        // match :host-context
 const host2 = /:host(?!-)(?:\(([\s\S]*?)\))*(\s|,|::)/gm;   // match :host
+const conflict = /^(?=\.)([\s\S]*?)(?: }| |,\n)/gm;
+const styles = {};
 
 function transform(name, content) {
     return content
@@ -15,6 +17,54 @@ function transform(name, content) {
         .replace(slotted3, `${name} slot $1`)
         .replace(host1, `$1 ${name}`)
         .replace(host2, `${name}$1$2`)
+}
+
+function checkConflict(name, content) {
+    const matches = content.matchAll(conflict);
+    for (const match of matches) {
+        const key = match[1];
+        let obj;
+        if (!styles[key]) {
+            obj = styles[key] = { total: 0 };
+        } else {
+            obj = styles[key];
+        }
+        if (!obj[name]) {
+            obj.total += 1;
+            obj[name] = 1;
+        } else {
+            obj[name] = obj[name] + 1;
+        }
+    }
+}
+
+function fixConflict(name, content, classNames) {
+    let c = content;
+    for (const className of classNames) {
+        const regex = new RegExp(`^(?=\.)(${className}[\s\S]*?)(?: }| |,\n)`, 'gm');
+        c = c.replace(regex, `${name} $1`);
+    }
+    return c;
+}
+
+function listConflictingStyles() {
+    const rootPath = './src/components';
+    const fileNames = fs.readdirSync(rootPath);
+    for (const dir of fileNames) {
+        const p = path.resolve(rootPath, dir);
+        if (!fs.statSync(p).isDirectory()) continue;
+        const files = fs.readdirSync(p);
+        for (const file of files) {
+            if (path.extname(file) !== '.scss') continue;
+            const content = fs.readFileSync(path.resolve(p, file), 'utf8');
+            checkConflict(dir, content);
+        }
+    }
+    for (const key in styles) {
+        if (styles[key].total > 1) continue;
+        delete styles[key];
+    }
+    console.log(styles);
 }
 
 function generateCss() {
@@ -27,34 +77,40 @@ function generateCss() {
             if (path.extname(fileName) !== component.include) {
                 continue;
             }
-            const specific = component.specifics.find(x => x.name === fileName);
             const srcFile = path.resolve(componentPath, fileName);
+            let file = fs.readFileSync(srcFile, 'utf8');
+            let tag = component.tag;
             let distFile = path.resolve(component.srcDir, fileName);
+            const specific = component.specifics.find(x => x.name === fileName);
             if (specific) {
+                // 重置文件名
                 if (specific.extname) {
-                    fileName = fileName.replace(path.extname(fileName), specific.extname);
                     distFile = distFile.replace(path.extname(distFile), specific.extname);
                 }
 
-                if (specific.transform) {
-                    const name = specific.suffix ? `${component.tag}${specific.suffix}` : component.tag;
-                    const file = fs.readFileSync(srcFile, 'utf8');
-                    const content = transform(name, file);
-                    console.log(distFile);
-                    fs.writeFileSync(distFile, content);
-                } else {
-                    fs.copyFileSync(srcFile, distFile);
+                // 重置tag
+                if (specific.suffix) {
+                    tag = `${component.tag}${specific.suffix}`;
                 }
-            } else {
-                fs.copyFileSync(srcFile, distFile);
+
+                // 转化格式
+                if (specific.transform) {
+                    file = transform(tag, file);
+                }
+
+                if (specific.conflict) {
+                    file = fixConflict(tag, file, specific.conflict);
+                }
             }
+            console.log(distFile);
+            fs.writeFileSync(distFile, file);
         }
     }
 
-    const context = {
-        path: 'src/css/components.scss',
-    }
-    execSync(`"./node_modules/.bin/hygen" generator style style --json=${Buffer.from(JSON.stringify(context)).toString('base64')}`);
+    // const context = {
+    //     path: 'src/css/components.scss',
+    // }
+    // execSync(`"./node_modules/.bin/hygen" generator style style --json=${Buffer.from(JSON.stringify(context)).toString('base64')}`);
 }
 
 function generateIcons() {
@@ -67,7 +123,9 @@ function generateIcons() {
 
 function generate() {
     generateCss();
-    generateIcons();
+    // generateIcons();
 }
 
 generate();
+
+// listConflictingStyles();
