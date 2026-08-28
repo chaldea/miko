b22965fcf0f3518f71cea059f90af19b7f8368f1
zhaowenkai <zhaowenkai@flexem.com>
2026-08-28 15:07:13 +0800

    fix(ionic): correct spinner layout and animations

--- tests/Miko.Ionic.Tests/Components/IonSpinnerTests.cs
+++ tests/Miko.Ionic.Tests/Components/IonSpinnerTests.cs
@@ -1,5 +1,6 @@
 using Miko.Animation;
 using Miko.Common;
+using Miko.Components;
 using Miko.Ionic.Components;
 using Shouldly;
 
@@ -102,6 +103,106 @@ public class IonSpinnerTests : IonicComponentTestBase
         style.Width.ShouldBe(Length.Px(28));
         style.Height.ShouldBe(Length.Px(28));
         style.Color.ShouldBe(Color.FromHex("0054e9"));
+    }
+
+    [Theory]
+    [InlineData("lines-small")]
+    [InlineData("lines-sharp-small")]
+    public void IonSpinner_SmallLinePreset_KeepsStandardHostSize(string name)
+    {
+        Context.AddStyleSheet(IonicStyleSheetFactory.CreateAllModes());
+
+        var cut = Context.Render<IonSpinner>(p => p.Add(nameof(IonSpinner.Name), name));
+        var style = cut.GetComputedStyle(cut.Root)!;
+
+        style.Width.ShouldBe(Length.Px(28));
+        style.Height.ShouldBe(Length.Px(28));
+    }
+
+    [Theory]
+    [InlineData("lines-small")]
+    [InlineData("lines-sharp-small")]
+    public void IonSpinner_SmallLinePreset_RotatesAroundHostCenter(string name)
+    {
+        Context.AddStyleSheet(IonicStyleSheetFactory.CreateAllModes());
+
+        var cut = Context.Render<IonSpinner>(p => p.Add(nameof(IonSpinner.Name), name));
+        var lineStyle = cut.GetComputedStyle(cut.FindByClass("spinner-line-1").Single())!;
+
+        lineStyle.Top.ShouldBe(Length.Px(2));
+        lineStyle.Left.ShouldBe(Length.Percent(50));
+        lineStyle.TransformOrigin.ShouldBe(
+            new TransformOrigin(Length.Percent(50), Length.Px(12)));
+    }
+
+    [Fact]
+    public void IonSpinner_InItemStartSlot_HasEndMargin()
+    {
+        Context.AddStyleSheet(IonicStyleSheetFactory.CreateAllModes());
+
+        RenderFragment spinner = builder =>
+        {
+            builder.OpenComponent<IonSpinner>(0);
+            builder.CloseComponent();
+        };
+        var cut = Context.Render<IonItem>(p => p.Add(nameof(IonItem.Start), spinner));
+        var spinnerElement = cut.FindByClass("ion-spinner").Single();
+
+        cut.GetComputedStyle(spinnerElement)!.MarginInlineEnd.ShouldBe(Length.Px(16));
+    }
+
+    [Fact]
+    public void IonSpinner_Dots_AnimatesSegmentsFromLeftToRight()
+    {
+        Context.AddStyleSheet(IonicStyleSheetFactory.CreateAllModes());
+
+        var cut = Context.Render<IonSpinner>(p => p.Add(nameof(IonSpinner.Name), "dots"));
+        var circles = cut.FindByClass("spinner-circle");
+
+        cut.Root.Style?.Animations.ShouldBeNull();
+        circles.Count.ShouldBe(3);
+        circles.Select(circle => cut.GetComputedStyle(circle)!.Left)
+            .ShouldBe(new[] { Length.Px(2), Length.Px(11), Length.Px(20) });
+        circles.Select(circle => circle.Style!.Animations!.Value.Value.ShouldHaveSingleItem().Name)
+            .ShouldAllBe(name => name == "ion-spinner-dots");
+        circles.Select(circle => circle.Style!.Animations!.Value.Value.Single().Delay)
+            .ShouldBe(new[] { -0.22f, -0.11f, 0f });
+    }
+
+    [Theory]
+    [InlineData("bubbles")]
+    [InlineData("circles")]
+    public void IonSpinner_RingPreset_RotatesSegmentsAroundHostCenter(string name)
+    {
+        var cut = Context.Render<IonSpinner>(p => p.Add(nameof(IonSpinner.Name), name));
+        var firstCircle = cut.FindByClass("spinner-circle-1").Single();
+
+        cut.Root.Style!.TransformOrigin!.Value.Value.ShouldBe(TransformOrigin.Center);
+        cut.Root.Style.Animations!.Value.Value.ShouldHaveSingleItem().Name.ShouldBe("ion-spinner-rotate");
+        firstCircle.Style!.Top!.Value.Value.ShouldBe(Length.Percent(50));
+        firstCircle.Style.Left!.Value.Value.ShouldBe(Length.Percent(82));
+        firstCircle.Style.Transform!.Value.Value.Functions.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void IonSpinner_Bubbles_ScalesEachBubbleWithStaggeredTiming()
+    {
+        var cut = Context.Render<IonSpinner>(p => p.Add(nameof(IonSpinner.Name), "bubbles"));
+        var bubbles = cut.FindByClass("spinner-circle");
+
+        bubbles.Count.ShouldBe(9);
+        bubbles.Select(bubble => bubble.Style!.Animations!.Value.Value.ShouldHaveSingleItem().Name)
+            .ShouldAllBe(name => name == "ion-spinner-scale-out");
+
+        var firstAnimation = bubbles[0].Style!.Animations!.Value.Value.Single();
+        firstAnimation.Delay.ShouldBe(-1f);
+        firstAnimation.Keyframes[0].Style.Transform!.Value.Value.Functions
+            .ShouldHaveSingleItem().ShouldBe(new TransformFunction.Scale(1f, 1f));
+        firstAnimation.Keyframes[1].Style.Transform!.Value.Value.Functions
+            .ShouldHaveSingleItem().ShouldBe(new TransformFunction.Scale(0f, 0f));
+
+        bubbles.Select(bubble => bubble.Style!.Animations!.Value.Value.Single().Delay)
+            .ShouldBeInOrder(SortDirection.Ascending);
     }
 
     [Fact]
